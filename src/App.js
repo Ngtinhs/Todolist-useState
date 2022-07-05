814bf8ce459779c71a062a9a0f956abd644483f4
Ngtinh <19dh110978@st.huflit.edu.vn>
2022-07-05 14:09:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [job, setJob] = useState("");
+  const [jobs, setJobs] = useState(() => {
+    const storageJobs = JSON.parse(localStorage.getItem("jobs"));
+    return storageJobs ?? [];
+  });
+
+  const handleSubmit = () => {
+    if (job.trim()) {
+      setJobs((prev) => {
+        const newJobs = [...prev, job];
+
+        // Save to local storage
+        const jsonJobs = JSON.stringify(newJobs);
+        localStorage.setItem("jobs", jsonJobs);
+
+        return newJobs;
+      });
+    }
+    //clear value on input
+    setJob("");
+  };
+
+  const handleDelete = (job) => {
+    setJobs((prev) => {
+      const newJobs = prev.filter((item) => item !== job);
+
+      // Save after deleting
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem("jobs", jsonJobs);
+
+      return newJobs;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "2rem" }}>
+      <h2>Todo List</h2>
+      <input
+        value={job}
+        onChange={(e) => setJob(e.target.value)}
+        onKeyPress={(e) => e.charCode === 13 && handleSubmit()}
+      />
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job}
+            <span
+              onClick={() => handleDelete(job)}
+              style={{ paddingLeft: 8, color: "red", cursor: "pointer" }}
+            >
+              x
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
